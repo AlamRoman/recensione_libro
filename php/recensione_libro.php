e54c1134d9bd9cd7dc1d6f2e3cb575b52acd223b
Roman <romanalam8083@gmail.com>
2025-02-26 09:36:52 +0100

    crea recensione

--- php/recensione_libro.php
+++ php/recensione_libro.php
@@ -48,7 +48,7 @@
     $OPERATION = ws_operation($_SERVER['REQUEST_URI']);
 
     $headers = getallheaders();
-    $token = $headers["Auth-Token"] ?? null;
+    $token = $headers["Auth-Token"] ?? null; //get the auth token frim the header
 
     $responseData = [];
     $status_code = 405;
@@ -59,11 +59,13 @@
             
             if ($token !== null && validate_token($token)) {
                 
+                //get all books from the database
                 $sql = "SELECT * FROM libro";
                 $stmt = $conn->prepare($sql);
                 $stmt->execute();
                 $result = $stmt->get_result();
 
+                //put all the books in the response
                 while($book = $result->fetch_assoc()){
                     $responseData[] = $book;
                 }
@@ -79,7 +81,7 @@
             }
 
         }else{
-            $statuscode = 404; //operation not found
+            $status_code = 404; //operation not found
 
             $responseData = [
                 "status"  => "error",
@@ -126,6 +128,7 @@
             
                 if (password_verify($password, $user["password"])) {
 
+                    //create auth token by hashing username and current time
                     $token = hash('sha256', $username . ':' . time());
 
                     $sql = "UPDATE users SET token = ? WHERE username = ?";
@@ -138,7 +141,7 @@
                         "message" => "Login successful",
                         "token"   => $token
                     ];
-                    $statuscode = 200; //OK
+                    $status_code = 200; //OK
                     
                 }else{
                     $responseData = [
@@ -156,8 +159,99 @@
                 $status_code = 401; // unauthorized
             }
 
-        }else{// not found
-            $statuscode = 404;
+        }else if ($OPERATION == "create_recensione") {
+
+            if ($token !== null && validate_token($token)) {
+
+                $input = file_get_contents("php://input");
+
+                $id_user = "";
+                $id_libro = "";
+                $voto = "";
+                $commento = "";
+                $data_ultima_modifica = "";
+
+                //prendi l'id del user con il suo token
+                $sql = "SELECT * FROM users WHERE token = ?";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("s", $token);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $user = $result->fetch_assoc();
+
+                $id_user = $user["id"];
+
+                $data_ultima_modifica = date('Y-m-d H:i:s');
+
+                if ($CONTENT_TYPE === "application/xml") {// xml data
+
+                    libxml_use_internal_errors(true);
+                    $xml = simplexml_load_string($input);
+
+                    if (!$xml) {
+                        http_response_code(400); // bad request
+                        header("Content-Type: application/xml");
+                        echo "<response><status>error</status><message>Invalid XML format</message></response>";
+                        exit;
+                    }
+
+                    $id_libro = (string) $xml->id_libro;
+                    $voto = (string) $xml->voto;
+                    $commento = (string) $xml->commento;
+                
+                }else if($CONTENT_TYPE === "application/json"){ //json data
+                    // TODO
+                }
+
+                $sql = "SELECT * FROM recensione WHERE id_user = ? AND id_libro = ?";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("ii", $id_user, $id_libro);
+                $stmt->execute();
+                $result = $stmt->get_result();
+
+                if ($result->num_rows == 0) {
+
+                    $sql = "INSERT INTO recensione(id_user, id_libro, voto, commento, data_ultima_modifica) VALUES(?,?,?,?,?)";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bind_param("iisss", $id_user,$id_libro, $voto, $commento, $data_ultima_modifica);
+                    
+                    if ($stmt->execute()) {
+                        
+                        $result = $stmt->get_result();
+
+                        $responseData = [
+                            "status"  => "success",
+                            "message" => "Recensione inserito con successo"
+                        ];
+                        $status_code = 200; //OK
+
+                    }else{
+
+                        $responseData = [
+                            "status"  => "error",
+                            "message" => "Errore nell'inserimento nel database"
+                        ];
+                        $status_code = 500; // internal server error
+                    }
+                    
+                }else{
+                    $responseData = [
+                        "status"  => "error",
+                        "message" => "Recensione gia presente per questo libro da questo utente"
+                    ];
+                    $status_code = 409; // conflict
+                }
+
+            }else{
+                $responseData = [
+                    "status"  => "error",
+                    "message" => "Unauthorized"
+                ];
+                $status_code = 401; // unauthorized
+            }
+
+        }else {// not found
+            $status_code = 404;
 
             $responseData = [
                 "status"  => "error",
@@ -174,8 +268,7 @@
 
         $responseData = [
             "status"  => "error",
-            "message" => "Method not allowed",
-            "token"   => $token
+            "message" => "Method not allowed"
         ];
     }
 
@@ -183,12 +276,17 @@
     http_response_code($status_code);
 
     if ($CONTENT_TYPE === "application/xml") {
+
         header("Content-Type: application/xml"); //set content type
         $xmlResponse = new SimpleXMLElement('<response/>'); //create response xml
         array_to_xml($responseData, $xmlResponse); //convert data to xml
+
         echo $xmlResponse->asXML(); //print the response
+
     } else {
+
         header("Content-Type: application/json"); //set content type
+
         echo json_encode($responseData); // encode and print the response
     }
 
